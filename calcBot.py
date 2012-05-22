2023917d5219e3f16e59ecdfb3232ed2c31a259d
Guillaume BINET <gbin@gootz.net>
2012-05-23 01:40:05 +0200

    added a warning if the qalc command is not found

--- calcBot.py
+++ calcBot.py
@@ -3,7 +3,7 @@
 from botplugin import BotPlugin
 from jabberbot import botcmd
 import subprocess
-
+CMD = '/usr/bin/qalc'
 class CalcBot(BotPlugin):
 
     @botcmd
@@ -13,6 +13,8 @@ class CalcBot(BotPlugin):
         !calc x²+x+1=5
         !calc 5 in² = x cm²
         """
+        if not os.path.exists(CMD):
+            return 'ERROR: this plugin requires libqalculate with the qalc command to be installed. On gentoo it requires the readline useflag.'
         if not args:
             return 'Please give me an expression to solve'
         p = subprocess.Popen(['/usr/bin/qalc', '-t', args], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
