21060b3ed0096dbb36612d79a2492b2791376029
Guillaume BINET <gbin@gootz.net>
2013-01-03 14:54:35 -0800

    py2 compatibility

--- tests/test_calcbot.py
+++ tests/test_calcbot.py
@@ -4,7 +4,7 @@ from errbot.backends.test import FullStackTest
 class TestCommands(FullStackTest):
     @classmethod
     def setUpClass(cls, extra=None):
-        super().setUpClass(__file__)
+        super(TestCommands, cls).setUpClass(__file__)
 
     def test_calc(self):
         self.assertCommand('!calc 2x²-x-1=0', 'x = 1 or x = -0.5')
